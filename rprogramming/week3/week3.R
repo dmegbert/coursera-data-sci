6fa53afbbc487fd8d53f6a4de902f9ef67cfe4ec
david.egbert <dmegbert@us.ibm.com>
2016-12-24 13:36:17 -0500

    Week 3 notes and sample code on loop functions

--- rprogramming/week3/week3.R
+++ rprogramming/week3/week3.R
@@ -37,4 +37,57 @@ apply(a, c(1,2), mean)
 #can also use rowMeans pass dims arg
 rowMeans(a, dims = 2)
 
+#mapply is a multivariate apply of sorts which applies a function in 
+#parallel over a set of arguments
 
+#mapply(FUN, ..., MoreArgs = NULL, SIMPLIFY = TRUE, USE.NAMES = TRUE)  
+#the ... is the arguments to apply over
+#MoreArgs is a list of ohter arguments to FUN
+#Simplify - should result be simlplified
+
+mapply(rep, 1:4, 4:1)
+
+noise <- function(n, mean, sd) {
+    rnorm(n, mean, sd)
+}
+noise(5,1,2)
+
+#vectorize a function that does not take in vector arguments using mapply
+mapply(noise, 1:5, 1:5, 2)
+
+#tapply applies function over subsets of a vector
+#tapply(X, INDEX, FUN=NULL, ..., simplify = TRUE)
+#X is a vector, INDEX is a factor or a list of Factors, FUNction, ... args to 
+#pass to the function, simplify - should results be simplified
+
+x <- c(rnorm(10), runif(10), rnorm(10, 1))
+f <- gl(3,10)
+f
+tapply(x, f, mean)
+
+#split takes a vector or other object and splits it into groups determined by a
+#factor or a list of factors
+#split(x, f, drop = FALSE, ...) x is vector/list/df, f is factor or list of factors
+# drop indicates whether empty levels should be dropped
+
+split(x, f)
+
+#common to use split and lapply together
+lapply(split)
+library(datasets)
+head(airquality)
+
+#find mean within each month
+s <- split(airquality, airquality$Month)
+lapply(s, function(x) colMeans(x[, c("Ozone", "Solar.R", "Wind")]))
+
+#get rid of NAs and simplify using sapply
+sapply(s, function(x) colMeans(x[, c("Ozone", "Solar.R", "Wind")], na.rm = TRUE))
+
+#Splitting on more than one level
+x <- rnorm(10)
+f1 <- gl(2, 5)
+f2 <- gl(5, 2)
+interaction(f1,f2)
+str(split(x, list(f1,f2)))
+str(split(x, list(f1,f2), drop = TRUE))
